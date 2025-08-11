9e45499eb582420ac21aa966fdce9a11d669af6f
alexispuca12 <alexis1alexis2alexis3@gmail.com>
2025-08-11 16:07:47 -0400

    configuracion de apps alexis

--- src/apps/horario/models.py
+++ src/apps/horario/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Alesi(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    dni = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.nombre}, {self.apellido}"
+
 # Create your models here.
