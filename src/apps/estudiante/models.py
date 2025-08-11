a753e618548d9418120e1df948f74e5eb5ff611a
Matias121106 <matiassalatino@ctpantoniomarte.edu.ar>
2025-08-11 17:07:18 -0300

    Modelo creado Estudiante

--- src/apps/estudiante/models.py
+++ src/apps/estudiante/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Estudiante(models.Model):
+	nombre = models.CharField(max_length=100)
+	profesor = models.CharField(max_length=100)
+	edad = models.IntegerField()
+
+	def __str__(self):
+		return self.nombre
