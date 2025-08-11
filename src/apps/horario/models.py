b66bfebcd22f2100dff197400574c401ef39fb59
alexispuca12 <alexis1alexis2alexis3@gmail.com>
2025-08-11 16:13:51 -0400

    arreglo de modelo horario

--- src/apps/horario/models.py
+++ src/apps/horario/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Alesi(models.Model):
+class Horario(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     dni = models.CharField(max_length=100)
