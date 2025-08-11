82d7a92c557607b6c77ee23d3e54bc7edd9ee513
ValeriaJael <jaelbela36@gmail.com>
2025-08-11 18:20:00 -0300

    agregue una lista en urls.py

--- src/core/urls.py
+++ src/core/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('docente/', include('apps.docente.urls'))
+    path('docente/', include('apps.docente.urls')),
 ]
