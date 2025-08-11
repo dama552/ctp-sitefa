43f0c125d1775d443fc784979f62147293d8df21
Matias121106 <matiassalatino@ctpantoniomarte.edu.ar>
2025-08-11 17:44:31 -0300

    Creado el archivo urls.py

--- src/apps/estudiante/models.py
+++ src/apps/estudiante/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Estudiante(models.Model):
-	nombre = models.CharField(max_length=100)
-	profesor = models.CharField(max_length=100)
-	edad = models.IntegerField()
-
-	def __str__(self):
-		return self.nombre
+	nombre=models.CharField(max_length=100)
+	profesor=models.CharField(max_length=100)
+	edad=models.IntegerField()
+	apellido=models.CharField(max_length=100)
+	
+	def __str__ (self):
+		return f"{self.nombre},{self.apellido}"
