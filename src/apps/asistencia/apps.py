c44edf1df15e7c153245c6af50b574b8e875fa46
dama552 <damarisvargas728@gmail.com>
2025-08-11 09:23:06 -0300

    agregue apps y settings

--- src/apps/asistencia/apps.py
+++ src/apps/asistencia/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AsistenciaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.sistencia'
+    name = 'apps.asistencia'
