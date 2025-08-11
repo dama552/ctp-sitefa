a963b802e3086bf82b489b17e1685f87f0419131
Palsichaca <palsichaca@gmail.com>
2025-08-11 21:59:13 +0200

    creacion clase

--- src/apps/materia/models.py
+++ src/apps/materia/models.py
@@ -1,7 +1,8 @@
 from django.db import models
  
-#class Materia (models.Model):
-
-    
+class Materia (models.Model):
+    nombre=models.Charfield(max_lenght=100)  
+    def __str__ (self):
+        return f"{self.nombre}"
 
 # Create your models here.
