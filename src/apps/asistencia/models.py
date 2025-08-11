bc378657caf8e1b41efdc04d0da9af8f5464d2f5
dama552 <damarisvargas728@gmail.com>
2025-08-11 17:07:51 -0300

    cree el modelo estudiante

--- src/apps/asistencia/models.py
+++ src/apps/asistencia/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 class Estudiante (models.Model):
     nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    dni = models.CharField(max_length=20)
+    
+    def __str__(self):
+          return self.nombre
     
 
 # Create your models here.
