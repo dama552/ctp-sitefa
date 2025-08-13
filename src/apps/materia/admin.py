2040cd644099d43c6eabec091bc1383d9b8629da
Palsichaca <palsichaca@gmail.com>
2025-08-13 16:40:36 +0200

    admin configuracion

--- src/apps/materia/admin.py
+++ src/apps/materia/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Materia
 
+@admin.register(Materia)
+class MateriaAdmin(admin.ModelAdmin):
+    search_fields = ("nombre")
+    list_filter = ("nombre")
+    ordering = ("nombre")
 # Register your models here.
