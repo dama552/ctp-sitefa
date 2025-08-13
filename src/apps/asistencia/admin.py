9943ac645b6552f6e68fd3349f806f9f8149cbdc
dama552 <damarisvargas728@gmail.com>
2025-08-13 11:48:09 -0300

    registre modelo en el admin.py

--- src/apps/asistencia/admin.py
+++ src/apps/asistencia/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Asistencia
 
 # Register your models here.
+class AsistenciaAdmin(admin.ModelAdmin):
+    list_display = ('fecha')
+    search_fields = ('fecha')
+    list_filter = ('fecha')
+    ordering = ('fecha')
