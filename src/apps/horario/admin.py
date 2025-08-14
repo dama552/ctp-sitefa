7c775a31e9e27b4d1da674b8c518e454fff76d43
alexispuca12 <alexis1alexis2alexis3@gmail.com>
2025-08-14 15:57:19 -0400

    registar horarios en admin

--- src/apps/horario/admin.py
+++ src/apps/horario/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Horario
+
+@admin.register(Horario)
+class HorarioAdmin(admin.ModelAdmin):
+    list_display = ['nombre']
+    search_fields = ['nombre']
+    list_filter = ['nombre']
+    ordering = ['nombre']
+
 
 # Register your models here.
+
