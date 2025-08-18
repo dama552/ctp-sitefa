46c68316709219b1ffd0bb61b628888a486b24b4
not-tofi <emirdiaz321@gmail.com>
2025-08-18 08:42:13 -0300

    agrege atributos a models.py

--- src/apps/calificacion/admin.py
+++ src/apps/calificacion/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Calificacion
 # Register your models here.
+@admin.register(Calificacion)
+class CalificacionAdmin(admin.ModelAdmin):
+	list_display = ('numero',)
+	search_fields = ('numero',)
+	list_filter = ('numero',)
+	ordering = ('numero',)
--- src/apps/calificacion/models.py
+++ src/apps/calificacion/models.py
@@ -3,7 +3,9 @@ from django.db import models
 #Crea tus modelos aquí.
 class Calificacion(models.Model):
     numero= models.DecimalField(max_digits=5, decimal_places=2)
+    materia =  models.CharField(max_length=100)
+    estudiante = models.CharField(max_length=100)
+    nota = models.CharField(max_length=100)
+    fecha = models.DateField(auto_now_add=True)
 
-
-    def __str__(self):
-        return f"{self.numero}"
+    # Agrega más campos según sea necesario
