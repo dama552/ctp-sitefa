9c92c654942153a9a0ef8de5b139218670190785
ValeriaJael <jaelbela36@gmail.com>
2025-08-18 08:22:54 -0300

    Agregue modelos a models

--- src/apps/docente/models.py
+++ src/apps/docente/models.py
@@ -4,6 +4,10 @@ class Docente(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     dni = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    telefono = models.IntegerField()
+    direccion = models.CharField(max_length=100)
+    fecha_Nacimiento = models.DateField()
     
     def __str__(self):
         return {self.nombre}, {self.apellido}
