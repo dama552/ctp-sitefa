80924706a7a55684d4b55a4f54b6338bd3b2a8f7
ValeriaJael <jaelbela36@gmail.com>
2025-08-18 18:18:22 -0300

    Agregue los templates, las vistas y urls

--- src/apps/docente/urls.py
+++ src/apps/docente/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from apps.docente.views import DocenteActualizarVista, DocenteCrearVista, DocenteEliminarVista, DocenteListaVista
 
 app_name = 'docente'
 urlpatterns = [
+    path ('docente/crear/', DocenteCrearVista.as_view(), name = 'crear-docente'),
+    path ('docente/lista/', DocenteListaVista.as_view(), name = 'list-docente'),
+    path ('docente/<int:pk>/editar/', DocenteActualizarVista.as_view(), name = 'actualizar-docente'),
+    path ('docente/<int:pk>/eliminar/', DocenteEliminarVista.as_view(), name = 'eliminar-docente'),
 ]
