7cbe94858c89cb2e2b581254ec0e98d13d5c0d79
alexispuca12 <alexis1alexis2alexis3@gmail.com>
2025-08-18 15:46:33 -0400

    agregado relacion materia y horario

--- src/apps/horario/models.py
+++ src/apps/horario/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Horario(models.Model):
-    materia = models.CharField(max_length=100)
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
     dia_semana = models.CharField(max_length=100) #agregar choice
-    hora_inicio = models.CharField(max_length=100)
-    hora_fin = models.CharField(max_length=100)
+    hora_inicio = models.CharField(max_length=100) #timefield probablemente
+    hora_fin = models.CharField(max_length=100) #timefield probablemente
     
     def __str__(self):
         return f"{self.materia}, {self.dia_semana}"
