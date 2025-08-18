86af3111bc537e4ea097208b1754474be56fcaa9
Palsichaca <palsichaca@gmail.com>
2025-08-18 13:33:51 +0200

    configuracion atributos

--- src/apps/materia/models.py
+++ src/apps/materia/models.py
@@ -2,7 +2,9 @@ from django.db import models
  
 class Materia (models.Model):
     nombre=models.CharField()  
+    anio=models.Charfield()
     def __str__ (self):
         return f"{self.nombre}"
+
 
 # Create your models here.
