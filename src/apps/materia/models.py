b7952a6a7f32ca45468fe986935dfadfc87117af
Palsichaca <palsichaca@gmail.com>
2025-08-18 21:47:26 +0200

    agregado relacion entre materia y profesorado

--- src/apps/materia/models.py
+++ src/apps/materia/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Materia (models.Model):
     nombre=models.CharField()  
     anio=models.CharField()
+    profesorado=models.ForeignKey(Profesorado,on_delete=models.CASCADE,related_name="materia")
 
     def __str__ (self):
         return self.nombre
