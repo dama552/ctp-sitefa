5a2fdea027f0bff6b20f699a23c907873a4907aa
Palsichaca <palsichaca@gmail.com>
2025-08-18 23:23:31 +0200

    creacion crud materia

--- src/apps/materia/urls.py
+++ src/apps/materia/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-
+from views import MateriaCrearVista,MateriaActualizarVista,MateriaEliminarVista,MateriaLeerVista
 
 app_name = 'materia'
 
 urlpatterns = [
+    path("materia/leer/",MateriaLeerVista.as_view(),name="materia_leer"),
+    path("materi/crear/",MateriaCrearVista.as_view(),name="materia_crear"),
+    path("materia/<int:pk>/actualizar/",MateriaActualizarVista.as_view(),name="materia_actualizar"),
+    path("materia/<int:pk>/eliminar/",MateriaEliminarVista.as_view(),name="materia_eliminar")
 ]
--- src/apps/materia/views.py
+++ src/apps/materia/views.py
@@ -1,3 +1,29 @@
+from django.views.generic import ListView, CreateView,UpdateView, DeleteView
+from django.urls import reverse_lazy
 from django.shortcuts import render
-
+from .models import Materia 
 # Create your views here.
+
+class MateriaLeerVista(ListView):
+    model = Materia
+    template_name = 'materia/leer.html'
+    context_object_name = 'materias'
+    
+
+class MateriaCrearVista(CreateView):
+    model = Materia
+    fields = '__all__'
+    template_name = 'materia/crear.html'
+    success_url = reverse_lazy('materia:materia_leer')
+    
+class MateriaActualizarVista(UpdateView):
+    model = Materia
+    fields = '__all__'
+    template_name = 'materia/actualizar.html'
+    success_url = reverse_lazy('materia:materia_leer')
+
+class MateriaEliminarVista(DeleteView):
+    model = Materia
+    fields = '__all__'
+    template_name = 'materia/eliminar.html'
+    success_url = reverse_lazy('materia:materia_leer')
