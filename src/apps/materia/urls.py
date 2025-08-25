6de85d9174a3e3f08054b789acda0df8bd41ef5d
Palsichaca <palsichaca@gmail.com>
2025-08-25 21:41:14 +0200

    correccion de rutas

--- src/apps/materia/urls.py
+++ src/apps/materia/urls.py
@@ -4,8 +4,8 @@ from .views import MateriaCrearVista,MateriaActualizarVista,MateriaEliminarVista
 app_name = 'materia'
 
 urlpatterns = [
-    path("materia/leer/",MateriaLeerVista.as_view(),name="materia_leer"),
-    path("materi/crear/",MateriaCrearVista.as_view(),name="materia_crear"),
-    path("materia/<int:pk>/actualizar/",MateriaActualizarVista.as_view(),name="materia_actualizar"),
-    path("materia/<int:pk>/eliminar/",MateriaEliminarVista.as_view(),name="materia_eliminar")
+    path("leer/",MateriaLeerVista.as_view(),name="materia_leer"),
+    path("crear/",MateriaCrearVista.as_view(),name="materia_crear"),
+    path("actualizar/<int:pk>/",MateriaActualizarVista.as_view(),name="materia_actualizar"),
+    path("eliminar/<int:pk>/",MateriaEliminarVista.as_view(),name="materia_eliminar")
 ]
