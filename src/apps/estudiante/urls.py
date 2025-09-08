e678267369e05be762f8c048b47c044a88ad3bcb
Matias121106 <matiassalatino@ctpantoniomarte.edu.ar>
2025-09-08 16:26:18 -0300

    creacion de interfaz grafica matias

--- src/apps/estudiante/urls.py
+++ src/apps/estudiante/urls.py
@@ -5,6 +5,6 @@ app_name = 'estudiante'
 urlpatterns = [ 
     path('crear/', EstudianteCrearVista.as_view(), name='Estudiante_crear'),
     path('eliminar/<int:pk>', EstudianteEliminarVista.as_view(), name='Estudiante_eliminar'),
-    path('leer/', EstudianteLeerVista.as_view(), name='Estudiante_leer'),
+    path('leer/', EstudianteLeerVista.as_view(), name='Estudiante_Leer'),
     path('actualizar/<int:pk>', EstudianteActualizarVista.as_view(), name='Estudiante_actualizar'),
 ]
