5a21768ddd559b8b0627509c520e2d17d5b8afc8
Palsichaca <palsichaca@gmail.com>
2025-10-06 14:45:57 +0200

    cambios html materia

--- src/apps/materia/views.py
+++ src/apps/materia/views.py
@@ -1,29 +1,46 @@
-from django.views.generic import ListView, CreateView,UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from django.shortcuts import render
-from .models import Materia 
-# Create your views here.
+from .models import Materia
+
 
 class MateriaLeerVista(ListView):
     model = Materia
     template_name = 'materia/leer.html'
     context_object_name = 'materias'
-    
+    paginate_by = 10  # opcional
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        selected_anio = self.request.GET.get('anio')
+
+        if selected_anio:
+            queryset = queryset.filter(anio=selected_anio)
+
+        return queryset.order_by('anio', 'nombre')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        anios = Materia.objects.values_list('anio', flat=True).distinct().order_by('anio')
+        context['anios'] = anios
+        context['selected_anio'] = self.request.GET.get('anio', '')
+        return context
+
 
 class MateriaCrearVista(CreateView):
     model = Materia
     fields = '__all__'
     template_name = 'materia/crear.html'
     success_url = reverse_lazy('materia:materia_leer')
-    
+
+
 class MateriaActualizarVista(UpdateView):
     model = Materia
     fields = '__all__'
     template_name = 'materia/actualizar.html'
     success_url = reverse_lazy('materia:materia_leer')
 
+
 class MateriaEliminarVista(DeleteView):
     model = Materia
-    fields = '__all__'
     template_name = 'materia/eliminar.html'
     success_url = reverse_lazy('materia:materia_leer')
