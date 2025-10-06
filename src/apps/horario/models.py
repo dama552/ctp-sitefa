5760b5c0900650b09c0fcf8b297f558776d2cd2b
alexispuca12 <alexis1alexis2alexis3@gmail.com>
2025-10-06 09:44:37 -0300

    cambiado modelo y templates(crear y leer) de app horario

--- src/apps/horario/models.py
+++ src/apps/horario/models.py
@@ -3,10 +3,13 @@ from django.db import models
 from apps.materia.models import Materia
 
 class Horario(models.Model):
-    materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
-    dia_semana = models.CharField(max_length=100) #agregar choice
-    hora_inicio = models.CharField(max_length=100) #timefield probablemente
-    hora_fin = models.CharField(max_length=100) #timefield probablemente
+
+    DIAS_SEMANA = [('LUN', 'Lunes'),('MAR', 'Martes'),('MIE', 'Miércoles'),('JUE', 'Jueves'),('VIE', 'Viernes'),('SAB', 'Sábado'),]
+
+    materia = models.ForeignKey("materia.Materia", on_delete=models.CASCADE)
+    dia_semana = models.CharField(max_length=3, choices=DIAS_SEMANA)
+    hora_inicio = models.TimeField()
+    hora_fin = models.TimeField()
     
     def __str__(self):
         return f"{self.materia}, {self.dia_semana}"
