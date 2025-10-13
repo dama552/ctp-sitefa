598607e9cbd07f7459aba8be4661b526186cae1c
Palsichaca <palsichaca@gmail.com>
2025-10-13 14:33:17 +0200

    mas cambios html materia

--- src/apps/materia/views.py
+++ src/apps/materia/views.py
@@ -7,22 +7,36 @@ class MateriaLeerVista(ListView):
     model = Materia
     template_name = 'materia/leer.html'
     context_object_name = 'materias'
-    paginate_by = 10  # opcional
+    paginate_by = 10  # cantidad de registros por página
 
     def get_queryset(self):
+        """
+        Retorna la lista de materias filtrada por año y/o nombre.
+        """
         queryset = super().get_queryset()
         selected_anio = self.request.GET.get('anio')
+        search_query = self.request.GET.get('q')
 
         if selected_anio:
             queryset = queryset.filter(anio=selected_anio)
 
+        if search_query:
+            queryset = queryset.filter(nombre__icontains=search_query)
+
         return queryset.order_by('anio', 'nombre')
 
     def get_context_data(self, **kwargs):
+        """
+        Agrega variables adicionales al contexto: lista de años,
+        año seleccionado y texto de búsqueda.
+        """
         context = super().get_context_data(**kwargs)
         anios = Materia.objects.values_list('anio', flat=True).distinct().order_by('anio')
+
         context['anios'] = anios
         context['selected_anio'] = self.request.GET.get('anio', '')
+        context['search_query'] = self.request.GET.get('q', '')
+
         return context
 
 
